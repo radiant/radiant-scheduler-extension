80cf552b896f82f5ee1e98154409c3aaa2712883
john muhl <git@johnmuhl.com>
2012-03-29 12:57:11 -0500

    require spec helper in a way that is 1.8 & 1.9 compatible

--- spec/models/page_extensions_spec.rb
+++ spec/models/page_extensions_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path '../../spec_helper', __FILE__
 
 describe "Scheduler::PageExtensions" do
   dataset :pages_with_scheduling
